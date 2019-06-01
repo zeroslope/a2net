874a1fdf5bf40f74c7a9d8853a5c5fd3a99ccd0f
Zeroslope <738543046@qq.com>
2019-06-01 23:35:17 +0800

    add DataLoader

---
+++ trainer/data_loader.py
@@ -0,0 +1,152 @@
+import os
+from os import path
+import pathlib
+import random
+import argparse
+import glog as log
+
+import tensorflow as tf
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+from config import global_config
+CFG = global_config.cfg
+
+
+def preprocess_image(image):
+    #TODO: add size
+    image = tf.image.decode_png(image, channels=3)
+    image_float = tf.image.convert_image_dtype(image, tf.float32)
+    image_yuv = tf.image.rgb_to_yuv(image_float)
+    return image_yuv
+
+
+def _process_rain_gt(record):
+    return (preprocess_image(record['rain']), preprocess_image(record['gt']))
+
+
+def augment_for_train(rain_image, clean_image):
+    rain_image = tf.cast(rain_image, tf.float32)
+    clean_image = tf.cast(clean_image, tf.float32)
+
+    return random_crop_batch_images(
+        rain_image=rain_image,
+        clean_image=clean_image,
+        cropped_size=[CFG.TRAIN.CROP_IMG_WIDTH, CFG.TRAIN.CROP_IMG_HEIGHT])
+
+
+def augment_for_test(rain_image, clean_image):
+    return rain_image, clean_image
+
+
+# def normalize(rain_image, clean_image):
+#     rain_image_fp = tf.concat([rain_image[:,:,:1] * 2 - 1, rain_image[:,:,1:] * 2])
+#     clean_image_fp = tf.concat([rain_image[:,:,:1] * 2 - 1, rain_image[:,:,1:] * 2])
+
+#     return rain_image_fp, clean_image_fp
+
+
+def random_crop_batch_images(rain_image, clean_image, cropped_size):
+    concat_images = tf.concat([rain_image, clean_image], axis=-1)
+
+    concat_cropped_images = tf.image.random_crop(
+        concat_images,
+        [cropped_size[1], cropped_size[0],
+         tf.shape(concat_images)[-1]],
+        seed=tf.random.set_random_seed(1234))
+
+    cropped_rain_image = tf.slice(concat_cropped_images,
+                                  begin=[0, 0, 0],
+                                  size=[cropped_size[1], cropped_size[0], 3])
+    cropped_clean_image = tf.slice(concat_cropped_images,
+                                   begin=[0, 0, 3],
+                                   size=[cropped_size[1], cropped_size[0], 3])
+
+    return cropped_rain_image, cropped_clean_image
+
+
+image_feature_description = {
+    'rain': tf.FixedLenFeature([], tf.string),
+    'gt': tf.FixedLenFeature([], tf.string),
+}
+
+
+def _parse_image_function(example_proto):
+    return tf.parse_single_example(example_proto, image_feature_description)
+
+
+class DataLoader(object):
+    def __init__(self, save_dir, flag='train'):
+        self._save_dir = save_dir
+
+        if not path.exists(self._save_dir):
+            raise ValueError('{:s} not exist, please check again'.format(
+                self._save_dir))
+
+        self._dataset_flags = flag.lower()
+        if self._dataset_flags not in ['train', 'test', 'val']:
+            raise ValueError(
+                'flags of the data feeder should be \'train\', \'test\', \'val\''
+            )
+
+    def inputs(self, batch_size):
+        """
+        dataset feed pipline input
+        :param batch_size:
+        :param num_epochs:
+        :return: A tuple (images, labels), where:
+                    * images is a float tensor with shape [batch_size, H, W, C]
+                      in the range [-0.5, 0.5].
+                    * labels is an int32 tensor with shape [batch_size] with the true label,
+                      a number in the range [0, CLASS_NUMS).
+        """
+        # if not num_epochs:
+        #     num_epochs = None
+
+        tfrecords_file_path = path.join(
+            self._save_dir, '{}.tfrecords'.format(self._dataset_flags))
+
+        with tf.name_scope('input_tensor'):
+            dataset = tf.data.TFRecordDataset(tfrecords_file_path)
+
+            dataset = dataset.map(
+                map_func=_parse_image_function,
+                num_parallel_calls=CFG.TRAIN.CPU_MULTI_PROCESS_NUMS)
+            dataset = dataset.map(
+                map_func=_process_rain_gt,
+                num_parallel_calls=CFG.TRAIN.CPU_MULTI_PROCESS_NUMS)
+            if self._dataset_flags != 'test':
+                dataset = dataset.map(
+                    map_func=augment_for_train,
+                    num_parallel_calls=CFG.TRAIN.CPU_MULTI_PROCESS_NUMS)
+            else:
+                dataset = dataset.map(
+                    map_func=augment_for_test,
+                    num_parallel_calls=CFG.TRAIN.CPU_MULTI_PROCESS_NUMS)
+            # dataset = dataset.map(
+            #     map_func=normalize,
+            #     num_parallel_calls=CFG.TRAIN.CPU_MULTI_PROCESS_NUMS)
+
+            # The shuffle transformation uses a finite-sized buffer to shuffle elements
+            # in memory. The parameter is the number of elements in the buffer. For
+            # completely uniform shuffling, set the parameter to be the same as the
+            # number of elements in the dataset.
+            if self._dataset_flags != 'test':
+                dataset = dataset.shuffle(buffer_size=1000)
+                # repeat num epochs
+                dataset = dataset.repeat(18)
+
+            dataset = dataset.batch(batch_size, drop_remainder=True)
+            dataset = dataset.prefetch(buffer_size=AUTOTUNE)
+
+            iterator = dataset.make_one_shot_iterator()
+
+        return iterator.get_next(
+            name='{:s}_IteratorGetNext'.format(self._dataset_flags))
+
+
+if __name__ == "__main__":
+    dl = DataLoader("/Users/zeroslope/ai/a2net/trainer/data/tfrecord", 'train')
+    r = dl.inputs(4)
+
+    print(r)
